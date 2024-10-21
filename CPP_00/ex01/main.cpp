73da73058c52e268d357a9a1d5bcb74f06db39e6
valentinefleith <valentine.fleith@gmail.com>
2024-10-21 23:01:03 +0200

    add user input

--- CPP_00/ex01/main.cpp
+++ CPP_00/ex01/main.cpp
@@ -1,5 +1,9 @@
 #include <iostream>
 
 int main() {
-	std::cout << "This is a phonebook" << std::endl;
+	std::cout << "Type a command: ADD, SEARCH or EXIT" << std::endl;
+	std::string command;
+	std::getline(std::cin, command);
+	std::cout << "You entered: " << command << std::endl;
+	return 0;
 }
