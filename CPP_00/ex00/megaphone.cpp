f891d12941a2910cafda8733dd264abe62982fa8
valentinefleith <valentine.fleith@gmail.com>
2024-10-22 13:29:05 +0200

    format and improvements

--- CPP_00/ex00/megaphone.cpp
+++ CPP_00/ex00/megaphone.cpp
@@ -1,16 +1,19 @@
 #include <cstring>
 #include <iostream>
 
-int main(int argc, char **argv) {
-  if (argc == 1) {
-    std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *\n";
-    return 0;
-  }
-  for (int i = 1; i < argc; i++) {
-    for (int j = 0; argv[i][j]; j++) {
-      std::cout << (char)std::toupper(argv[i][j]);
+void yell(const std::string& input) {
+    for (int i = 0; input[i]; i++)
+        std::cout << static_cast<char>(std::toupper(input[i]));
+}
+
+int main(int argc, char** argv) {
+    if (argc == 1) {
+        std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *\n";
+        return 0;
     }
-  }
-  std::cout << std::endl;
-  return 0;
+
+    for (int i = 1; i < argc; i++)
+        yell(argv[i]);
+    std::cout << std::endl;
+    return 0;
 }
--- CPP_00/ex01/main.cpp
+++ CPP_00/ex01/main.cpp
@@ -1,9 +1,10 @@
+#include <cstring>
 #include <iostream>
 
 int main() {
-	std::cout << "Type a command: ADD, SEARCH or EXIT" << std::endl;
-	std::string command;
-	std::getline(std::cin, command);
-	std::cout << "You entered: " << command << std::endl;
-	return 0;
+    std::cout << "Type a command: ADD, SEARCH or EXIT" << std::endl;
+    std::string command;
+    std::getline(std::cin, command);
+    std::cout << "You entered: " << command << std::endl;
+    return 0;
 }
