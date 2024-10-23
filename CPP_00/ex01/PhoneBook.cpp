0a71517b6a49fb2b449c4f0c650c71dd2d34f764
valentinefleith <valentine.fleith@gmail.com>
2024-10-23 15:52:36 +0200

    funct to create null contact

--- CPP_00/ex01/PhoneBook.cpp
+++ CPP_00/ex01/PhoneBook.cpp
@@ -1,5 +1,11 @@
 #include "PhoneBook.hpp"
 
+//PhoneBook::PhoneBook() {
+//     for (int i = 0; i < CONTACT_NB; i++) {
+//        contacts[i] = create_null_contact();
+//    }
+//}
+
 int PhoneBook::get_contact_index(const Contact contact) {
     for (int i = 0; i < 8; i++) {
         if (contact == contacts[i])
@@ -9,3 +15,8 @@ int PhoneBook::get_contact_index(const Contact contact) {
 }
 
 void PhoneBook::add_contact(const Contact contact) {}
+
+const Contact PhoneBook::create_null_contact() {
+    const Contact null_contact = Contact("", "", "", "", "");
+    return null_contact;
+}
--- CPP_00/ex01/PhoneBook.hpp
+++ CPP_00/ex01/PhoneBook.hpp
@@ -4,6 +4,8 @@
 #include <iostream>
 #include "Contact.hpp"
 
+#define CONTACT_NB 8
+
 class PhoneBook {
    public:
     PhoneBook();
@@ -11,9 +13,10 @@ class PhoneBook {
     void print_contact(const Contact contact);
 
    private:
-    const Contact contacts[8];
+    const Contact contacts[CONTACT_NB];
     int get_contact_index(const Contact contact);
     int get_nb_contact_stored();
+    const Contact create_null_contact();
 };
 
 #endif
